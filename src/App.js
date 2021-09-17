8745e9132eec5edda0ef5fef32d0ecab8bf0196a
coderzzp <coderzzp@gmail.com>
2021-09-17 22:29:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+
+const fistDayTime = 1597593600000
+const now = new Date().getTime()
+const diffinit = ~~((now-fistDayTime )/1000 )
 
 function App() {
+  const [diff,setDiff] = useState(diffinit)
+  useEffect(()=>{
+    const id = setTimeout(()=>{
+      setDiff(diff+1)
+    },1000)
+    return ()=>{
+      clearTimeout(id)
+    }
+  },[diff])
+  const day = ~~(diff/(24 * 60 * 60))
+  const hour = ~~((diff % (24 * 60 * 60) )/(60*60))
+  const minute =~~(((diff % (24 * 60 * 60) )%(60*60))/60)
+  const seconds =~~(((diff % (24 * 60 * 60) )%(60*60))%60)
+  const show = (time)=>{
+    if(time.toString().length<=1){
+      return `0${time}`
+    }else{
+      return time
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <img src={'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202002%2F08%2F20200208134336_MWhRd.thumb.400_0.gif&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1634401498&t=4362295d3dd269df6945e841c669101d'}></img>
+      <p> zzp ❤ xqy </p>
+       {day}D{show(hour)}H{show(minute)}M{show(seconds)}S
+      
     </div>
   );
 }
